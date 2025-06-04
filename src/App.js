f25f49bf72ec1e9307a2201afadbc4f6cc6be83f
Voste254 <onyangosteveok@gmail.com>
2025-06-04 17:06:55 +0300

    Modifications

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-     
-<div className='min-h-screen bg-purple-200 text-center' >
-<button className="btn ">Click me</button><br/>
-<span className="underline underline-white-100 decoration-slice bg-gradient-to-b from-yellow-400 to-red-500 text-black px-1">
-  Hello<br/>
-  World
-</span>
+    <>
+    <Navbar/>
+    </>  
 
-
-</div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='bg-blue-800 font-mono px-4 h-16 '>
+  <div className='flex items-center content-center justify-between h-full'>
+    <div>
+      <Link to='/' className='text-4xl font-semibold text-white'>LOGO</Link>
+    </div>
+    <ul className="text-xl [&>*]:inline-block text-white font-bold [&>*]:mr-6">
+      <li className=' '>
+        <Link to='/'>Home</Link>
+      </li>
+            <li className=''>
+        <Link to='/'>About Us</Link>
+      </li>
+            <li className=''>
+        <Link to='/'>Contacts</Link>
+      </li>
+          <button className='btn'><Link to='/'>Login</Link></button>
+          <button className='btn'><Link to='/'>Signup</Link></button>
+    </ul>
+
+  </div>
+</nav>
+<p className='oldstyle-nums'>123456789</p>
+    </div>
+  )
+}
+
+export default Navbar
